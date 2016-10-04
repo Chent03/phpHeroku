00fee93ce8f3df629baec40d990f53774d394874
Tony Chen <tochen1231@gmail.com>
2016-10-04 02:03:34 -0400

    change

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <?php
+ 
+// ==== Control Vars =======
+$strFromNumber = "+13473345340";
+$strToNumber = "+16465731388";
+$strMsg = "Hi from Twilio"; 
+$aryResponse = array();
+ 
+
+    // include the Twilio PHP library - download from 
+    // http://www.twilio.com/docs/libraries/
+    require_once ("inc/Services/Twilio.php");
+ 
+    // set our AccountSid and AuthToken - from www.twilio.com/user/account
+    $AccountSid = "AC030c146a257548c6e811ed307c02cbdc";
+    $AuthToken = "65b24d71e1b4e8c14539ee0039c280a4";
+ 
+    // ini a new Twilio Rest Client
+    $objConnection = new Services_Twilio($AccountSid, $AuthToken);
+
+
+    // Send a new outgoinging SMS by POSTing to the SMS resource */
+    $bSuccess = $objConnection->account->sms_messages->create(
+        
+        $strFromNumber, 	// number we are sending From 
+        $strToNumber,           // number we are sending To
+        $strMsg			// the sms body
+    );
+
+		
+    $aryResponse["SentMsg"] = $strMsg;
+    $aryResponse["Success"] = true;
     
-    require_once('twilio-php-master/Services/Twilio.php'); // Loads the library
-    $version = "2010-04-01"
-    $sid = "AC030c146a257548c6e811ed307c02cbdc"; // Your Account SID from www.twilio.com/console
-    $token = "65b24d71e1b4e8c14539ee0039c280a4"; // Your Auth Token from www.twilio.com/console
-
-    $client = new Services_Twilio($AccountSid, $AuthToken, $version); //initialise the Twilio client
-    try{
-        $message = $client->account->messages->create(array(
-        "From" => "+13473345340",
-        "To" => "+16465731388",
-        "Body" => "Test message!",
-        ));
-
-        // Display a confirmation message on the screen
-        echo "Sent message";
-    }catch (Exception $e) {
-            echo 'Error: ' . $e->getMessage();
-        }
-?>
+    
+    echo json_encode($aryResponse);
