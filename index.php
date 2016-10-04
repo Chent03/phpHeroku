57081af0ee46b2243537b2715a9fb709e5994485
Tony Chen <tochen1231@gmail.com>
2016-10-04 01:57:33 -0400

    version

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
     $sid = "AC030c146a257548c6e811ed307c02cbdc"; // Your Account SID from www.twilio.com/console
     $token = "65b24d71e1b4e8c14539ee0039c280a4"; // Your Auth Token from www.twilio.com/console
 
-    $client = new Services_Twilio($account_sid, $auth_token); 
- 
-    $client->account->messages->create(array( 
-	   'To' => "+16465731388", 
-	   'From' => "+13473345340", 
-	   'Body' => "Hi from twilio"
-    ));
+    $client = new Services_Twilio($AccountSid, $AuthToken, $version); //initialise the Twilio client
+    try{
+        $message = $client->account->messages->create(array(
+        "From" => "+13473345340",
+        "To" => "+16465731388",
+        "Body" => "Test message!",
+        ));
 
-print $message->sid;
+        // Display a confirmation message on the screen
+        echo "Sent message";
+    }catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
 ?>
